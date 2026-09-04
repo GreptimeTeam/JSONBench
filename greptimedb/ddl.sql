e9782a5fa7c6e9e1166d423e83cec0aadf24204f
luofucong <luofc@foxmail.com>
2026-09-04 17:31:27 +0800

    new create table

--- greptimedb/ddl.sql
+++ greptimedb/ddl.sql
@@ -1,7 +1,16 @@
 CREATE TABLE bluesky (
-    "data" JSON2,
+    "data" JSON2 (
+        max_auto_expanded_paths = 0,
+        kind String,
+        commit.operation String,
+        commit.collection String,
+        did String,
+        time_us Int64,
+    ),
     time_us TimestampMicrosecond TIME INDEX
 ) WITH (
+    'compaction.type' = 'twcs',
+    'compaction.twcs.trigger_file_num' = '100000000',
     'append_mode' = 'true',
     'sst_format' = 'flat'
 )
